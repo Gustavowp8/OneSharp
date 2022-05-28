84f9a1b48d454c2fb7d0ba92574825f0f53b4ae0
Gustavo Ferreira <gustavoferreira73@live.com>
2022-05-28 19:46:52 -0300

    envio da pagina inicial

---
+++ pages/funcao/app.js
@@ -0,0 +1,8 @@
+const menu = document.getElementById('menu')
+
+function mMenu(){
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+}
+
+menu.addEventListener('click', mMenu)
