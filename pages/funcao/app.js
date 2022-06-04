9cd41e419189c2ee2f2aeebc94a46e053cf85dd7
Gustavo Ferreira <gustavoferreira73@live.com>
2022-06-04 09:32:01 -0300

    Janela popup adicionado

--- pages/funcao/app.js
+++ pages/funcao/app.js
@@ -12,3 +12,17 @@ menu.addEventListener('click', mMenu)
 function irPerfil(){
     location='prestador.html'
 }
+/*======= compra pacote =======*/
+function chamacompra(){
+    confirCompraPacote.style.display='block'
+}
+
+function fecharcompra(){
+    confirCompraPacote.style.display='none'
+}
+
+const comprar = document.getElementById('confirmarCompra')
+comprar.addEventListener('click', realizarcompra)
+function realizarcompra(){
+    alert('Indisponivel no momento')
+}
